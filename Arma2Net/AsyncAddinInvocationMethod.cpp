c5ac9595e700b14c4dbc1bcaa4571670cb30255c
ScottNZ <stealtharcher.scott@gmail.com>
2014-03-11 02:14:16 +1300

    Properly fix string comparison in AsyncAddinInvocationMethod and add exception propagation

--- Arma2Net/AsyncAddinInvocationMethod.cpp
+++ Arma2Net/AsyncAddinInvocationMethod.cpp
@@ -14,10 +14,11 @@
 * limitations under the License.
 */
 
+#include "Utils.h"
 #include "Addin.h"
 
 using namespace System;
-using namespace System::Collections::Generic;
+using namespace System::Collections::Concurrent;
 using namespace System::Threading::Tasks;
 
 namespace Arma2Net
@@ -26,13 +27,26 @@ namespace Arma2Net
 	{
 	private:
 		initonly Addin^ addin;
-		initonly Queue<String^>^ results = gcnew Queue<String^>;
+		initonly ConcurrentQueue<String^>^ results = gcnew ConcurrentQueue<String^>;
+		Exception^ exception;
 
 		void InvokeImpl(Object^ obj)
 		{
 			auto tuple = (Tuple<String^, int>^)obj;
 			auto result = addin->Invoke(tuple->Item1, tuple->Item2);
 			results->Enqueue(result);
+		}
+
+		bool HandleException(Exception^ e)
+		{
+			exception = e;
+			return true;
+		}
+
+		String^ TaskFaulted(Task^ task)
+		{
+			task->Exception->Handle(gcnew Func<Exception^, bool>(this, &AsyncAddinInvocationMethod::HandleException));
+			return nullptr;
 		}
 	public:
 		AsyncAddinInvocationMethod(Addin^ addin)
@@ -42,14 +56,24 @@ namespace Arma2Net
 
 		virtual String^ Invoke(String^ args, int maxResultSize)
 		{
-			if (args->Equals("getresult", StringComparer::OrdinalIgnoreCase))
+			if (args != nullptr && args->Equals("getresult", StringComparison::OrdinalIgnoreCase))
 			{
-				if (results->Count > 0)
-					return results->Dequeue();
-				return nullptr;
+				if (exception != nullptr)
+				{
+					Exception^ e = exception;
+					exception = nullptr;
+					throw e;
+				}
+					
+				String^ result;
+				results->TryDequeue(result);
+				return result;
 			}
+
 			auto tuple = Tuple::Create(args, maxResultSize);
-			Task::Factory->StartNew(gcnew Action<Object^>(this, &AsyncAddinInvocationMethod::InvokeImpl), tuple);
+			auto task = gcnew Task(gcnew Action<Object^>(this, &AsyncAddinInvocationMethod::InvokeImpl), tuple);
+			task->ContinueWith(gcnew Func<Task^, String^>(this, &AsyncAddinInvocationMethod::TaskFaulted), TaskContinuationOptions::OnlyOnFaulted);
+			task->Start();
 			return nullptr;
 		}
 	};
